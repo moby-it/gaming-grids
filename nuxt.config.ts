8e894e241eeaae56993a0b1477c123bd7805e481
Stefanos <spalaio@moby-it.com>
2024-06-05 20:15:54 +0300

    Supabase url and key
    Deleted the .env file.
    Since the url and key are safe to be public info I moved them to the nuxt.config.ts file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   modules: ["@vueuse/nuxt", "@nuxt/image", '@nuxtjs/supabase'],
   devtools: { enabled: true },
   supabase: {
+    url: "https://znvtpipzflqwytxrtatb.supabase.co",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpudnRwaXB6Zmxxd3l0eHJ0YXRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk3NDQ2MjksImV4cCI6MjAyNTMyMDYyOX0.GR_EKQT3J8BaOrJMgTAhziM41FqHx7sPo3RaBaF32zA",
     redirect: false
   }
 });
