303fffd63d68960e8c4865b6b29d804fe5058bfc
Stefanos <spalaio@moby-it.com>
2024-07-31 11:49:59 +0300

    leaderboard
    created leadeboard page
    modified the navbar to handle routing to the leaderboard page

---
+++ pages/[[puzzleDate]]/leader-board.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { SupabaseClient } from '@supabase/supabase-js';
+
+const supabase: SupabaseClient = useSupabaseClient();
+const route = useRoute();
+const puzzleDate = (route.query.puzzleDate as string) ?? getCurrentDate();
+const { data: puzzleId } = await fetchPuzzleIdByDate(supabase, puzzleDate);
+if (!puzzleId.value) throw createError('failed to fetch puzzle');
+const { user } = useAuth();
+
+// const puzzleStore = usePuzzleStore();
+// const { name, guesses, status } = storeToRefs(puzzleStore);
+// if (!name.value) await puzzleStore.loadPuzzle(puzzleId.value);
+
+// const mostPopularStore = useMostPopularStore();
+// const { championNames, championIds, rarityScores, loading } = storeToRefs(mostPopularStore);
+// if (status.value === 'completed') await mostPopularStore.loadMostPopular(puzzleId.value);
+
+// onMounted(async () => {
+//     if (!user.value && puzzleId.value) {
+//         const puzzle = await getLocalPuzzle(puzzleId.value);
+//         guesses.value = puzzle.guesses;
+//         if (status.value === 'completed')
+//             await mostPopularStore.loadMostPopularClient(puzzleId.value);
+//     }
+// });
+
+// const selectedCell = ref<Cell>({ x: -1, y: -1 });
+// provide('selectedCell', selectedCell);
+</script>
+<template>
+    <h1>Leaderboard</h1>
+</template>
+<style scoped>
+</style>
